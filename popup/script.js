7f99b53dd0dfc61d9f2701e20e56ab1ca55c8793
Luke Carrier <luke.carrier@babylonhealth.com>
2021-11-28 12:05:12 +0000

    Provide better feedback during and after operations

--- popup/script.js
+++ popup/script.js
@@ -2,13 +2,31 @@ const KEYCODE_ENTER = 13;
 
 const URL_SEPARATOR = "\n";
 
+function disableButton(button, inProgressHtml) {
+  const originalHtml = button.innerHTML;
+
+  button.disabled = true;
+  button.innerHTML = inProgressHtml;
+
+  return doneHtml => {
+    button.innerHTML = doneHtml;
+
+    setTimeout(() => {
+      button.disabled = false;
+      button.innerHTML = originalHtml;
+    }, 500);
+  };
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   const openTabsForm = document.querySelector("#open-tabs");
   const openTabsUrlList = openTabsForm.querySelector("[name='url-list']");
   const openTabsTabGroup = openTabsForm.querySelector("[name='tab-group']");
+  const openTabsSubmitButton = openTabsForm.querySelector("[name='submit']");
 
   const copyUrlsForm = document.querySelector("#copy-urls");
   const copyUrlsTabGroup = copyUrlsForm.querySelector("[name='tab-group']");
+  const copyUrlsSubmitButton = copyUrlsForm.querySelector("[name='submit']");
 
   const tabGroupSelects = [openTabsTabGroup, copyUrlsTabGroup];
 
@@ -32,19 +50,11 @@ window.addEventListener("DOMContentLoaded", () => {
 
   openTabsForm.addEventListener("submit", e => {
     e.preventDefault();
+    const enableButton = disableButton(openTabsSubmitButton, "Opening tabs...");
 
     const urls = openTabsUrlList.value.split(URL_SEPARATOR);
     const tabIds = [];
     const groupId = parseInt(openTabsTabGroup.value, 10);
-
-    const maybeCreateTabGroup = groupId === chrome.tabGroups.TAB_GROUP_ID_NONE ?
-      null : tab => {
-        tabIds.push(tab.id);
-
-        if (tabIds.length === urls.length) {
-          chrome.tabs.group({ groupId, tabIds });
-        }
-      };
 
     urls.forEach(url => {
       url = url.trimLeft().trimRight();
@@ -56,17 +66,27 @@ window.addEventListener("DOMContentLoaded", () => {
         // Don't steal the user's focus.
         active: false,
         url,
-      }, maybeCreateTabGroup);
+      }, tab => {
+        tabIds.push(tab.id);
+        if (tabIds.length === urls.length) {
+          if (groupId !== chrome.tabGroups.TAB_GROUP_ID_NONE) {
+            chrome.tabs.group({ groupId, tabIds })
+          }
+          enableButton("Opened!");
+        }
+      });
     });
   });
 
   copyUrlsForm.addEventListener("submit", e => {
     e.preventDefault();
+    const enableButton = disableButton(copyUrlsSubmitButton, "Copying URLs...");
 
     const groupId = parseInt(copyUrlsTabGroup.value, 10);
     chrome.tabs.query({ groupId }, tabs => {
       const urls = tabs.map(tab => tab.url).join(URL_SEPARATOR);
       navigator.clipboard.writeText(urls);
+      enableButton("Copied!")
     });
   });
 });
